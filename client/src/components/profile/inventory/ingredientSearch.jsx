eaba0338b7cb54fc5ca97a4ba8b169253e49ae63
bill haigh <williamhaigh297@gmail.com>
2025-01-09 10:57:02 +0000

    FIX: ingredient search working as intended

--- client/src/components/profile/inventory/ingredientSearch.jsx
+++ client/src/components/profile/inventory/ingredientSearch.jsx
@@ -21,20 +21,20 @@ function IngredientSearch() {
   }
 
   function handleChange(event) {
-    console.log(event.target.value)
     setSearchText(event.target.value.toLowerCase());
-    const filteredList = fullIngredientList.filter((el) => {
-      console.log(el.strIngredient1.toLowerCase().includes(searchText))
-      if (el.strIngredient1.toLowerCase().includes(searchText)) return el;
-      //return el.strIngredient1.toLowerCase().includes(searchText);
-    })
+  }
+  
+  function filterList() {
+    const filteredList = fullIngredientList.filter((el) => el.strIngredient1.toLowerCase().includes(searchText)
+    )
     setFilteredIngredientList(filteredList);
   }
 
   useEffect(() => {
     getInventory();
     getFullIngredientList();
-  }, []);
+    filterList();
+  }, [searchText]);
 
   return (
     <>
@@ -42,7 +42,7 @@ function IngredientSearch() {
     <p>Search and Add ingredients to your list!</p>
     <input type="text" onChange={handleChange}></input>
     <div>
-    {(/*filteredIngredientList.length < 100 &&*/ filteredIngredientList.length > 0) ? (
+    {(filteredIngredientList.length < 20 && filteredIngredientList.length > 0) ? (
         filteredIngredientList.map((ingredient) => {
             return (
                 <Ingredient key={ingredient.strIngredient1} ingredient={ingredient} inventory={inventory} setInventory={setInventory}></Ingredient>

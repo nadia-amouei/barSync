7696f156f9165011ab10fea57663d34f6d0970dd
bill haigh <williamhaigh297@gmail.com>
2025-01-09 11:56:10 +0000

    CHORE: basic server added, dependencies installed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
 //TODO: basic server structure
+"use strict";
+
+const express = require("express");
+const router = require("./router.js");
+const cors = require("cors");
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`[SERVER]: server running at http://localhost:${PORT}`);
+});
--- server/router.js
+++ server/router.js
@@ -1,1 +1,4 @@
 //TODO: basic router
+const router = require("express").Router();
+
+module.exports = router;

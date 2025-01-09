f09d836bf119ea42e82d70975b05d5a32fb9f85d
bill haigh <williamhaigh297@gmail.com>
2025-01-09 11:30:50 +0000

    FEAT: added button functionality to ingredients

--- client/src/components/nav-bar/nav-bar.jsx
+++ client/src/components/nav-bar/nav-bar.jsx
@@ -1,9 +1,13 @@
 //TODO: nav bar component
 
+import { Link } from "react-router"
+
 function Navbar() {
   
   return (
     <>
+    <Link to={'/'}><button>home</button></Link>
+
       <p>navbar</p>
     </>
   )
--- client/src/components/profile/inventory/ingredient.jsx
+++ client/src/components/profile/inventory/ingredient.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
 function Ingredient({ ingredient, inventory, setInventory}) {
-  
+  const [added, setAdded] = useState(false);
+
+
+  const plainTextInventory = inventory.map((el) => el.strIngredient1)
+
+  useEffect(() => {
+    if (inventory.length && plainTextInventory.includes(ingredient.strIngredient1)) {
+      setAdded(true);
+    } else {
+      setAdded(false);
+    }
+  }, [inventory])
+
+  function addIngredient() {
+    const inventoryCopy = inventory.slice();
+    inventoryCopy.push(ingredient);
+    setInventory(inventoryCopy);
+  }
   
   function removeIngredient () {
     const idxOfIngredient = inventory.indexOf(ingredient);
@@ -10,7 +29,7 @@ function Ingredient({ ingredient, inventory, setInventory}) {
 //TODO: add a toggle for add / remove ingredient this is going to be re-used for the ingredient search 
   return (
     <>
-      <div>{ingredient.strIngredient1}</div><button onClick={removeIngredient}>X</button>
+      <div>{ingredient.strIngredient1}</div><button onClick={added ? removeIngredient : addIngredient}>{added ? "X" : "+"}</button>
     </>
   )
 }

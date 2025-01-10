21f862e6208b741e6ca401670036e95759964f8e
bill haigh <williamhaigh297@gmail.com>
2025-01-10 10:09:20 +0000

    FEAT: recipe detail includes list of ingredients and ability to add / remove them from inventory

--- client/src/components/profile/recipes/recipe-detail.jsx
+++ client/src/components/profile/recipes/recipe-detail.jsx
@@ -3,14 +3,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Navbar from "../../nav-bar/nav-bar";
+import Ingredient from "../inventory/ingredient";
 
 function RecipeDetail() {
   const [drinkDetail, setDrinkDetail] = useState(null);
+  const [drinkIngredients, setDrinkIngredients] = useState([]);
+  const [inventory, setInventory] = useState([]);
 
   let params = useParams();
   const drinkId = params.recipeId;
 
   useEffect(() => {
+    if (!inventory.length) {
+      getInventory();
+    }
     getDrinkDetails(drinkId);
   }, []);
 
@@ -20,7 +26,29 @@ function RecipeDetail() {
       const response = await fetch(url + drinkId);
       const fetchResponse = await response.json();
       if (fetchResponse.drinks.length) {
-        setDrinkDetail(fetchResponse.drinks[0]);
+        const cocktailRecipe = fetchResponse.drinks[0];
+        const cocktailIngredients = [];
+        for (const key in cocktailRecipe) {
+          if (key.includes("strIngredient") && cocktailRecipe[key] !== null) {
+            cocktailIngredients.push({ strIngredient1: cocktailRecipe[key] });
+          }
+        }
+        setDrinkDetail(cocktailRecipe);
+        setDrinkIngredients(cocktailIngredients);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getInventory() {
+    const url = "http://localhost:3000/inventory";
+    console.log("get inventory triggered");
+    try {
+      const response = await fetch(url);
+      const fetchInventory = await response.json();
+      if (fetchInventory.length) {
+        setInventory(fetchInventory);
       }
     } catch (error) {
       console.log(error);
@@ -36,6 +64,21 @@ function RecipeDetail() {
           <h1>{drinkDetail.strDrink}</h1>
           <img src={drinkDetail.strDrinkThumb} />
           <p>{drinkDetail.strInstructions}</p>
+          {drinkIngredients.length ? (
+            drinkIngredients.map((ingredient) => {
+              return (
+                <Ingredient
+                  key={ingredient.strIngredient1}
+                  ingredient={ingredient}
+                  inventory={inventory}
+                  setInventory={setInventory}
+                  getInventory={getInventory}
+                />
+              );
+            })
+          ) : (
+            <p>no ingredients?</p>
+          )}
         </>
       ) : (
         <p>None found...</p>

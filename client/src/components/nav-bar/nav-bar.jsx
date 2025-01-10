28e73da442ac96eb6dbbce40b8ea5847b5bb16aa
bill haigh <williamhaigh297@gmail.com>
2025-01-10 11:33:16 +0000

    STYLE: initial nav-bar style and global colors

--- client/src/components/nav-bar/nav-bar.jsx
+++ client/src/components/nav-bar/nav-bar.jsx
@@ -5,22 +5,22 @@ import { Link } from "react-router";
 function Navbar() {
   return (
     <>
-    <div className="navbar">
-      <div className="hero">
-        <h1 className="title">barSync</h1>
+      <div className="navbar">
+        <div className="hero">
+          <h1 className="title">barSync</h1>
+        </div>
+        <div className="nav-buttons">
+          <Link to={"/"} className="nav-button">
+            <p>Inventory</p>
+          </Link>
+          <Link to={"/ingredientsearch"} className="nav-button">
+            <p>Add Ingredient</p>
+          </Link>
+          <Link to={"/recipes"} className="nav-button">
+            <p>Make something?</p>
+          </Link>
+        </div>
       </div>
-      <div className="nav-buttons">
-      <Link to={"/"} className="nav-button">
-        <p>Inventory</p>
-      </Link>
-      <Link to={"/ingredientsearch"} className="nav-button">
- <p>Add Ingredient</p>
-      </Link>
-      <Link to={"/recipes"} className="nav-button">
-        <p>Make something?</p>
-      </Link>
-      </div>
-    </div>
     </>
   );
 }
